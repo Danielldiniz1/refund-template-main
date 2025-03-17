c787e398485159a36903562445300dab4aae8bfd
Danielldiniz1 <fortedinizdaniel@gmail.com>
2025-03-17 01:59:00 -0300

    Formatando o input de valor da despesa no padra BRL

--- script.js
+++ script.js
@@ -7,6 +7,23 @@ const amount = document.querySelector('#amount');
 
 //Capturando o evento de input do campo de valor
 amount.oninput = () => {
+    //Obtem o valor atual do input e remove todos os caracteres não numéricos
     let value = amount.value.replace(/\D/g, '');
-    amount.value = value;
+
+    //Transforma o valor em centavos
+    value = Number(value) / 100;
+    
+    //Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value);
 }
+
+function formatCurrencyBRL(value) {
+    //Formata o valor para o padrão brasileiro
+    value = value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+    return value;
+}
+
+
